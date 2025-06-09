d3646c0d05a8d6399e3f1db671a3b5f4bcb4cbdc
varram04 <84831000+varram04@users.noreply.github.com>
2025-06-09 09:39:28 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="public/projects/varun resume.pdf"
+                href="/projects/varun resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
